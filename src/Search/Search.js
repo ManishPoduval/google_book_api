075aaf95bc9898f3982fa173df138cc2a14b147f
Manish Poduval <manishpoduval@hotmail.com>
2020-05-16 06:30:24 +0530

    Propose changes

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -8,14 +8,20 @@ export default class Search extends Component {
             query: "",
         };
     }
+    
+    updateInput = (event) => {
+        event.preventDefault();
+        this.setState({query: event.value})
+    }
 
     render() {
         return (
             <div>
                 <input
+                    onChange={this.updateInput}
                     placeholder="Search"
                 />
-                <button onClick={this.props.setQuery(this.state.query)}>Search</button>
+                <button onClick={() => {this.props.setQuery(this.state.query)}}>Search</button>
             </div>
         )
     }
